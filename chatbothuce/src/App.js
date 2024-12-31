8ae63ce9d12366d4dab2989489c6897f79f6f25b
nghiant <nghiant9608@gmail.com>
2024-12-31 14:48:04 +0700

    config docker react

--- chatbothuce/src/App.js
+++ chatbothuce/src/App.js
@@ -34,7 +34,7 @@ function App() {
     setIsLoading(true); // Bắt đầu loading
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/api/chat', {
+      const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/chat`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
